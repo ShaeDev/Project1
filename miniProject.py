cce351c43491db920469b727686dc4233899618f
ShaeDev <shaeyan.jarvis@gmail.com>
2023-01-30 17:13:19 +0000

    the beginning of the code

--- miniProject.py
+++ miniProject.py
@@ -1,1 +1,13 @@
-python testscores
+fname= input('Enter first name: ')
+lname= input('Enter last name: ') 
+
+def userName():
+    fname = input("first name: ")
+    lname = input("last name: ")
+    print(fname,lname)
+
+def score(int):
+    testscore = int(input)
+    if int <= 100:
+     print(int + "%")
+
