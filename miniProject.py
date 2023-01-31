21bf7636c830e914cadf4c671457fb7fc1b51179
ShaeDev <shaeyan.jarvis@gmail.com>
2023-01-31 21:45:29 +0000

    Created classes for the first name, last name and scores of each student

--- miniProject.py
+++ miniProject.py
@@ -1,13 +1,33 @@
-fname= input('Enter first name: ')
-lname= input('Enter last name: ') 
+class firstName:
+    stu1 = "Abed"
+    stu2 = "Annie"
+    stu3 = "Britta"
+    stu4 = "Jeff"
+    stu5 = "Pierce"
+    stu6 = "Shirley"
+    stu7 = "Troy"
 
-def userName():
-    fname = input("first name: ")
-    lname = input("last name: ")
-    print(fname,lname)
+fName = firstName()
 
-def score(int):
-    testscore = int(input)
-    if int <= 100:
-     print(int + "%")
+class lastName:
+    stu1 = "Nader"
+    stu2 = "Eddison"
+    stu3 = "Perry"
+    stu4 = "Winger"
+    stu5 = "Hawthorne"
+    stu6 = "Bennet"
+    stu7 = "Barnes"
 
+lName = lastName()
+
+class score:
+    stu1 = 78
+    stu2 = 95
+    stu3 = 64
+    stu4 = 75
+    stu5 = 70
+    stu6 = 74
+    stu7 = 69
+print (firstName.stu4)
+print (lastName.stu4)
+print (score.stu4)
